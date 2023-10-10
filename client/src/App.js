17c555ff921f53bc5a69b4c82a28fb1589e1cd14
miriamYom <m0527669018@gmail.com>
2023-10-10 23:02:18 +0300

    changes...

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,31 @@
 import './App.css';
-import store  from './redux/store';
+import store from './redux/store';
 import { Provider } from 'react-redux';
-import { Home } from './homePage/homePage';
+import { HomePage } from './homePage/HomePage';
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Host from './host/Host';
-import Guest from './GuestComponents/js/Guest';
+import Guest from './GuestComponents/Guest';
 import Login from './loginWithAuth/Login';
 import { Header } from './Header';
 
 function App() {
   return (
+
     <>
-    
-      <header className="App-header">
-        <> 
-        <Header></Header>
-        <Provider store={store}>
+      <Header></Header>
+      <Provider store={store}>
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<Login />} />
-                          <Route path="/host" element={<Host />} />
+            <Route path="/host" element={<Host />} />
             <Route path="/guest" element={<Guest />} />
           </Routes>
 
         </Router>
 
       </Provider>
-
-        </>
     </>
   );
 }
