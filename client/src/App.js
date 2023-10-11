8d1545fc94e5e816076b3ec6f0baa002e998bf72
pessiMargalit <pessimargalit@gmail.com>
2023-10-11 13:00:12 +0300

    Change the icon to a heart image

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,24 @@
 import './App.css';
-import store  from './redux/store';
+import store from './redux/store';
 import { Provider } from 'react-redux';
-import { Home } from './homePage/homePage';
 import * as React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Host from './host/Host';
-import Guest from './GuestComponents/js/Guest';
+import Guest from './GuestComponents/Guest';
 import Login from './loginWithAuth/Login';
+import { HomePage } from './homePage/homePage';
 import { Header } from './Header';
 
 function App() {
   return (
     <>
-    
-      <header className="App-header">
-        <> 
-        <Header></Header>
-        <Provider store={store}>
+      <Header></Header>
+      <Provider store={store}>
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<Login />} />
-                          <Route path="/host" element={<Host />} />
+            <Route path="/host" element={<Host />} />
             <Route path="/guest" element={<Guest />} />
           </Routes>
 
@@ -29,7 +26,6 @@ function App() {
 
       </Provider>
 
-        </>
     </>
   );
 }
